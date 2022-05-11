9c915ac6d73becb1ae5e20444d0b10ba7c096096
thainamatos <thainamatos.if@hotmail.com>
2022-05-11 16:44:23 -0300

    js finalizado

--- main.js
+++ main.js
@@ -23,37 +23,48 @@ window.addEventListener('load', () => {
         taskEl.appendChild(taskContentEl); //insere um novo nó na estrutura DOM; acrescenta ao elemento selecionado o que for declarado dentro dos parenteses  
 
         const taskInputEl = document.createElement("input");
-        taskInputEl.classList.add("text");
+        taskInputEl.classList.add("text"); //add class para ja inserir os estilos  
         taskInputEl.type = "text";
-        taskInputEl.value = task;
-        taskInputEl.setAttribute("readonly", "readonly");
+        taskInputEl.value = task; //valor inserido no input é a task
+        taskInputEl.setAttribute("readonly", "readonly"); 
 
         taskContentEl.appendChild(taskInputEl);
 
         const taskActionsEl = document.createElement("div");
-        taskActionsEl.classList.add("actions");
+        taskActionsEl.classList.add("actions"); //edit e delete
 
         const taskEditEl = document.createElement("button");
         taskEditEl.classList.add("edit");
-        taskEditEl.innerHTML = "Edit";
+        taskEditEl.innerHTML = "Edit"; //
 
         const taskDeleteEl = document.createElement("button");
         taskDeleteEl.classList.add("delete");
         taskDeleteEl.innerHTML = "Delete";
 
-        taskActionsEl.appendChild(taskEditEl);
+        taskActionsEl.appendChild(taskEditEl); //inserindo dentro das divs
         taskActionsEl.appendChild(taskDeleteEl);
 
         taskEl.appendChild(taskActionsEl);
 
-        listEl.appendChild(taskEl);
+        listEl.appendChild(taskEl); // insere o conteudo da task dentro da lista
 
-        input.value = "";
+        input.value = ""; //"limpa" o input apos adicionar uma nova task
 
         taskEditEl.addEventListener('click', () => {
-            taskInputEl.removeAttribute("readonly"); //permite que seja alterado/editado dentro do input
-            taskInputEl.focus();
-            taskEditEl.innerText = "Save"; //altera de EDIT para SAVE enquanto a task estiver sendo editada
+            if (taskEditEl.innerText.toLowerCase() == "edit") {
+                taskInputEl.removeAttribute("readonly"); //permite que seja alterado/editado dentro do input
+                taskInputEl.focus();
+                taskEditEl.innerText = "Save"; //altera de EDIT para SAVE enquanto a task estiver sendo editada
+            } else {
+                taskInputEl.setAttribute("readonly", "readonly");
+                taskEditEl.innerText = "Edit";
+            }
+        })
+
+        taskDeleteEl.addEventListener('click', () => {
+           if(confirm("Are you sure you want to delete this task?")){ // cria um pop up na tela
+               listEl.removeChild(taskEl); //remove task inserida
+           }
         })
     })
 })
